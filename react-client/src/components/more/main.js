c5c61ecf8ed7b1a20c4165360ce658f8843a577f
JessicaShalomG <jsphxusa072@gmail.com>
2018-09-11 21:34:46 -0500

    api working not done

--- react-client/src/components/more/main.js
+++ react-client/src/components/more/main.js
@@ -1,9 +1,6 @@
 import React from "react";
-import Title from "./more";
 import Form from "./from";
 import Weather from "./weather";
-
-const KEY = "e560fec7749a1c67c58c3ed77444f238";
 
 class Main extends React.Component {
   constructor(props){
@@ -23,22 +20,36 @@ class Main extends React.Component {
     e.preventDefault();
     const city= e.target.elements.city.value;
     const country= e.target.elements.country.value;
-    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${KEY}&units=metric');
+    const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=f4a9036302d02ec9c3137278337d8c03&units=metric');
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error:""
-    });
+
+    if (city && country){
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error:""
+      });
+    }else{
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error:"Please enter values"
+      });
+    }
   }
   render(){
     return(
       <div>
-      <Title />
+
+      <h2>find the weather </h2>
+
       <Form getWeather={this.getWeather}/>
       <Weather
         temperature={this.state.temperature}

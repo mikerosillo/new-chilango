eeaab9cd2178a36bebd4f5265a330ec1e349cfb9
mike <angelrosillo2018@gmail.com>
2018-09-14 01:19:57 -0500

    port

--- server/index.js
+++ server/index.js
@@ -73,6 +73,6 @@ console.log(req.body)
  //  app.get('*', (req, res) => {
  //   res.sendFile(path.resolve(__dirname + '/../react-client/dist/index.html'));
  // });
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 8000, function() {
   console.log(`app is runnig on port ${process.env.PORT}`);
 })
